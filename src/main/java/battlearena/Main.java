35ab3381d943c4e8038b2daaf2c6b2e20f522dfd
antalpeti <antalpeti@users.noreply.github.com>
2025-05-19 08:47:48 +0200

    Add Enemy class and main method to initialize and display enemy attributes

---
+++ src/main/java/battlearena/Main.java
@@ -0,0 +1,11 @@
+package battlearena;
+
+public class Main {
+
+  @SuppressWarnings("java:S106")
+  public static void main(String[] args) {
+    Enemy zombie = new Enemy();
+    zombie.typeOfEnemy = "Zombie";
+    System.out.println(zombie.typeOfEnemy + " has " + zombie.healthPoints + " health points and " + zombie.attackDamage + " attack damage.");
+  }
+}
