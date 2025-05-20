9fff0d00f3f019db1de7233ac6f30a584bc7eb5f
antalpeti <antalpeti@users.noreply.github.com>
2025-05-20 08:44:01 +0200

    Refactor Enemy class to encapsulate attributes and add getter/setter methods; update Main class to use new methods

--- src/main/java/battlearena/Main.java
+++ src/main/java/battlearena/Main.java
@@ -5,8 +5,8 @@ public class Main {
   @SuppressWarnings("java:S106")
   public static void main(String[] args) {
     Enemy zombie = new Enemy();
-    zombie.typeOfEnemy = "Zombie";
-    System.out.println(zombie.typeOfEnemy + " has " + zombie.healthPoints + " health points and " + zombie.attackDamage + " attack damage.");
+    zombie.setTypeOfEnemy("Zombie");
+    System.out.println(zombie.getTypeOfEnemy() + " has " + zombie.getHealthPoints() + " health points and " + zombie.getAttackDamage() + " attack damage.");
     zombie.talk();
     zombie.walkForward();
     zombie.attack();
